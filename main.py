b1d0ae365ac79bb508d588e3181e512fd9ce4551
Martin Miglio <marmig0404@gmail.com>
2023-06-02 15:50:02 -0400

    update main

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import time
 import math
 
 
-def calcPi():
-    pi = sum(math.pow(-1, i) / (2 * i + 1) for i in range(100000000))
+def calcPi(count):
+    pi = sum(math.pow(-1, i) / (2 * i + 1) for i in range(count))
     pi *= 4
     return pi
 
 
 start = time.time()
-pi = calcPi()
+pi = calcPi(100000000)
 end = time.time()
 print(pi)
 print(f"Time taken: {str(end - start)} seconds")
